925c9a77ffae660d1679fd8dd8d7985bc318e3a2
Asif Ahmed <asif.ahmed@freshworks.com>
2019-07-24 17:46:35 +0530

    Add Label Component

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,7 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+import { postcss } from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
 
 export const config: Config = {
   namespace: 'freshworks-ui-kit',
@@ -14,5 +17,11 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null // disable service workers
     }
+  ],
+  plugins: [
+    sass(),
+    postcss({
+      plugins: [autoprefixer()]
+    })
   ]
 };
