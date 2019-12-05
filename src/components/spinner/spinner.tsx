b7fe1025520990e3b8b72befa9a7d7275d4b8e2c
Asif Ahmed <asif.ahmed@freshworks.com>
2019-12-05 14:30:07 +0530

    [MP-5895]- Spinner Fix

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -34,14 +34,8 @@ export class Spinner {
                   height: `${diameter}px`,
                 }
               }
-              viewBox={`0 0 ${diameter} ${diameter}`}>
-            <circle
-              class="path"
-              cx={diameter / 2}
-              cy={diameter / 2}
-              r={diameter / 2 - 3}
-              fill="none"
-              stroke-width={diameter / 8}></circle>
+              viewBox={`0 0 50 50`}>
+              <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
           </svg>;
   }
 }
