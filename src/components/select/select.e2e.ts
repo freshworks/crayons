340d62a2075dc85978998c98bb463a3bb7d4ba49
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2020-03-24 17:03:10 +0530

    E2E tests for select, input and radio group

--- src/components/select/select.e2e.ts
+++ src/components/select/select.e2e.ts
@@ -8,4 +8,95 @@ describe('fw-select', () => {
     const element = await page.find('fw-select');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('it checks if value can be set and get', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-select label="Select the house" required="true">
+                                <fw-select-option value="starks">Starks</fw-select-option>
+                                <fw-select-option value="lannisters">Lannisters</fw-select-option>
+                            </fw-select>`);
+
+    const element = await page.find('fw-select');
+
+    element.setAttribute('value', 'starks');
+
+    await page.waitForChanges();
+    const value = element.getAttribute('value');
+    expect(value).toBe('starks');
+
+  });
+
+  it('it checks if multiple values can be set and get', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-select multiple label="Select the house" required="true">
+                                <fw-select-option value="starks">Starks</fw-select-option>
+                                <fw-select-option value="lannisters">Lannisters</fw-select-option>
+                                <fw-select-option value="sands">Sands</fw-select-option>
+                            </fw-select>`);
+
+    const element = await page.find('fw-select');
+
+    element.setAttribute('value', ['starks', 'sands']);
+
+    await page.waitForChanges();
+    const value = element.getAttribute('value');
+    expect(value).toBe('starks,sands');
+
+  });
+
+  it('it emits fwBlur when the focus is changed away from the component', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-select label="Select the house" required="true" value="1">
+                                  <fw-select-option value="starks">Starks</fw-select-option>
+                                  <fw-select-option value="2">Lannisters</fw-select-option>
+                              </fw-select>`);
+    const fwBlur = await page.spyOnEvent('fwBlur');
+    const element = await page.find('fw-select');
+
+    await element.click();
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+
+    await page.waitForChanges();
+
+    expect(fwBlur).toHaveReceivedEvent();
+  });
+
+  it('it emits fwChange when the value is changed in the component', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-select label="Select the house" required="true" value="1">
+                              <fw-select-option value="starks">Starks</fw-select-option>
+                              <fw-select-option value="lannisters">Lannisters</fw-select-option>
+                            </fw-select>`);
+    const fwChange = await page.spyOnEvent('fwChange');
+    const element = await page.find('fw-select');
+
+    element.setProperty('value', 'lannisters');
+
+    await page.waitForChanges();
+
+    expect(fwChange).toHaveReceivedEventDetail({ 'value': 'lannisters' });
+  });
+
+  it('it emits fwFocus when the focus is on the component', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-select label="Select the house" required="true" value="1">
+                              <fw-select-option value="starks">Starks</fw-select-option>
+                              <fw-select-option value="lannisters">Lannisters</fw-select-option>
+                          </fw-select>`);
+    const fwFocus = await page.spyOnEvent('fwFocus');
+    const element = await page.find('fw-select');
+
+    await element.click();
+    await page.keyboard.press('Tab');
+
+    await page.waitForChanges();
+
+    expect(fwFocus).toHaveReceivedEvent();
+  });
 });
