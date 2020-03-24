48d46cee203be14af1347fa31a0ae63b960dd56e
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2020-03-24 17:03:10 +0530

    E2E tests for select, input and radio group

--- src/components/radio-group/radio-group.e2e.ts
+++ src/components/radio-group/radio-group.e2e.ts
@@ -8,4 +8,75 @@ describe('fw-radio-group', () => {
     const element = await page.find('fw-radio-group');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('it emits when input radigroup', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-radio-group>
+    <fw-radio value="yes">Yes</fw-radio>
+    <fw-radio value="no">No</fw-radio>
+    <fw-radio value="maybe">Maybe</fw-radio>
+  </fw-radio-group>`);
+
+    const fwChange = await page.spyOnEvent('fwChange');
+    const fwSelect = await page.spyOnEvent('fwSelect');
+    const fwFocus = await page.spyOnEvent('fwFocus');
+    const fwBlur = await page.spyOnEvent('fwBlur');
+
+    const element = await page.find('fw-radio-group');
+
+    await element.click();
+    element.setProperty('value', 'maybe');
+    await page.keyboard.press('Tab');
+
+    await page.waitForChanges();
+
+    expect(fwFocus).toHaveReceivedEvent();
+    expect(fwChange).toHaveReceivedEvent();
+    expect(fwSelect).toHaveReceivedEvent();
+    expect(fwBlur).toHaveReceivedEvent();
+
+  });
+
+  it('it checks the radio group value', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-radio-group>
+    <fw-radio value="yes">Yes</fw-radio>
+    <fw-radio value="no">No</fw-radio>
+    <fw-radio value="maybe">Maybe</fw-radio>
+  </fw-radio-group>`);
+
+    const fwChange = await page.spyOnEvent('fwChange');
+    const element = await page.find('fw-radio-group');
+
+    await element.click();
+    element.setProperty('value', 'maybe');
+    await page.keyboard.press('Tab');
+
+    await page.waitForChanges();
+
+    expect(fwChange).toHaveReceivedEventDetail({ 'value': 'maybe' });
+
+  });
+
+  it('it checks if value can be set', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-radio-group>
+    <fw-radio ch value="yes">Yes</fw-radio>
+    <fw-radio value="no">No</fw-radio>
+    <fw-radio value="maybe">Maybe</fw-radio>
+  </fw-radio-group>`);
+
+    const element = await page.find('fw-radio-group');
+
+    element.setAttribute('value', 'maybe');
+
+    await page.waitForChanges();
+    const valueAfterSetting = element.getAttribute('value');
+    expect(valueAfterSetting).toBe('maybe');
+
+  });
+
 });
