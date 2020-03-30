68cdc0fb90bcecd89058cb969e3c4e91a0d026da
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2020-03-30 19:29:59 +0530

    tests for textarea, tab, tabs

--- src/components/tab/tab.e2e.ts
+++ src/components/tab/tab.e2e.ts
@@ -8,4 +8,15 @@ describe('fw-tab', () => {
     const element = await page.find('fw-tab');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('Shows Html', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-tab></fw-tab>`);
+    const element = await page.find('fw-tab');
+
+    expect(element.shadowRoot).toEqualHtml(`<div>
+    <slot />
+  </div>`);
+  });
 });
--- src/components/textarea/textarea.e2e.ts
+++ src/components/textarea/textarea.e2e.ts
@@ -8,4 +8,76 @@ describe('fw-textarea', () => {
     const element = await page.find('fw-textarea');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('it checks if the input value can be read', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<fw-textarea value="tyrion lannister"></fw-textarea>');
+    const element = await page.find('fw-textarea');
+    const value = await element.getProperty('value');
+    expect(value).toBe('tyrion lannister');
+  });
+
+  it('It checks if the input value can be set', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<fw-textarea></fw-textarea>');
+    const element = await page.find('fw-textarea');
+    element.setProperty('value', 'tyrion lannister');
+
+    await page.waitForChanges();
+
+    const value = await element.getProperty('value');
+    expect(value).toBe('tyrion lannister');
+  });
+
+  it('it checks if the input has label', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<fw-textarea label="Name"> </fw-textarea>');
+    const element = await page.find('fw-textarea');
+    const label = element.getAttribute('label');
+
+    expect(label).toBe('Name');
+  });
+
+  it('it checks if the input has label', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<fw-input label="Name"> </fw-input>');
+    const element = await page.find('fw-input');
+    const label = element.getAttribute('label');
+
+    expect(label).toBe('Name');
+  });
+
+  it('it emits fwBlur when the focus is changed away from the component', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<fw-textarea value="1">1</fw-textarea>');
+    const fwBlur = await page.spyOnEvent('fwBlur');
+    const element = await page.find('fw-textarea');
+
+    await element.click();
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+
+    await page.waitForChanges();
+    expect(fwBlur).toHaveReceivedEvent();
+  });
+
+  it('it emits fwChange when the value is changed in the component', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<fw-textarea value="1"> </fw-textarea>');
+    const fwChange = await page.spyOnEvent('fwChange');
+    const element = await page.find('fw-textarea');
+
+    element.setProperty('value', '2');
+
+    await page.waitForChanges();
+
+    expect(fwChange).toHaveReceivedEventDetail({ 'value': '2' });
+  });
+
 });
