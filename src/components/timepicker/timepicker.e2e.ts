34e9a18766ee86d78096d9b6473eda3b142cf0eb
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2020-03-30 21:52:13 +0530

    tests for timpicker

--- src/components/timepicker/timepicker.e2e.ts
+++ src/components/timepicker/timepicker.e2e.ts
@@ -8,4 +8,35 @@ describe('fw-timepicker', () => {
     const element = await page.find('fw-timepicker');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('it emits fwBlur when the focus is changed away from the component', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-timepicker></fw-timepicker>`);
+    const fwBlur = await page.spyOnEvent('fwBlur');
+    const element = await page.find('fw-timepicker');
+
+    await element.click();
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+
+    await page.waitForChanges();
+
+    expect(fwBlur).toHaveReceivedEvent();
+  });
+
+  it('it emits fwFocus when the focus is on the component', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-timepicker></fw-timepicker>`);
+    const fwFocus = await page.spyOnEvent('fwFocus');
+    const element = await page.find('fw-timepicker');
+
+    await element.click();
+    await page.keyboard.press('Tab');
+
+    await page.waitForChanges();
+
+    expect(fwFocus).toHaveReceivedEvent();
+  });
 });
