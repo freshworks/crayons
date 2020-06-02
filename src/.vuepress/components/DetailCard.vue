081cc39e8978746ff5c5dc19146a8eed4a3106e9
Asif Ahmed <asif.ahmed@freshworks.com>
2020-06-02 11:13:00 +0530

    docs: stylint config changes

--- src/.vuepress/components/DetailCard.vue
+++ src/.vuepress/components/DetailCard.vue
@@ -39,153 +39,175 @@ export default {
 
 <style lang="scss">
 .link-card {
-    text-decoration: none;
-    text-align: center;
-    .card-container {
+  text-decoration: none;
+  text-align: center;
+
+  .card-container {
     height: 420px;
     width: 100%;
     border-radius: 15px;
-    background-color: #FFFFFF;
+    background-color: #fff;
     box-shadow: 0 5px 20px 0 rgba(57, 115, 230, 0.1);
     cursor: pointer;
     border: 1px solid transparent;
     transition: transform 500ms;
     box-sizing: border-box;
+
     .img-container {
-        text-align: center;
-        align-items: center;
-        img {
-            width: 100%;
-            height: 100%;
-        }
+      text-align: center;
+      align-items: center;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
     }
+
     .card-title {
-        font-size: 18px;
-        color: #4D4D4D;
-        font-weight: 500;
-        padding-top: 15px;
+      font-size: 18px;
+      color: #4d4d4d;
+      font-weight: 500;
+      padding-top: 15px;
     }
+
     .card-description {
-         font-family: National-Book;
-         color: #666666;
-         font-size: 15px;
-         letter-spacing: 0;
-         line-height: 28px;
-         padding: 15px;
+      font-family: 'National-Book', sans-serif;
+      color: #666;
+      font-size: 15px;
+      letter-spacing: 0;
+      line-height: 28px;
+      padding: 15px;
     }
+
     .learn-more {
-        font-family: National-Medium;
-        margin-top: 10px;
-        .learn-more-text {
-            color: #3973E6;
-            font-size: 15px;
-        }
+      font-family: 'National-Medium', sans-serif;
+      margin-top: 10px;
+
+      .learn-more-text {
+        color: #3973e6;
+        font-size: 15px;
+      }
     }
+
     &:hover {
-        transform: translateY(-25px);
-        box-shadow: 0 10px 30px 0 rgba(57, 115, 230, 0.15);
-        border-color: rgba(57, 115, 230, 0.2);
+      transform: translateY(-25px);
+      box-shadow: 0 10px 30px 0 rgba(57, 115, 230, 0.15);
+      border-color: rgba(57, 115, 230, 0.2);
     }
   }
 }
 
-@media only screen and (max-width: 48rem) { 
-.link-card {
-text-decoration: none;
-text-align: center;
-.card-container {
-    height: 420px;
-    width: 80%;
-    margin: 0 auto;
-    border-radius: 15px;
-    background-color: #FFFFFF;
-    margin-top: 5%;
-    box-shadow: 0 5px 20px 0 rgba(57, 115, 230, 0.1);
-    cursor: pointer;
-    border: 1px solid transparent;
-    transition: transform 500ms;
-    box-sizing: border-box;
-    .img-container {
+@media only screen and (max-width: 48rem) {
+  .link-card {
+    text-decoration: none;
+    text-align: center;
+
+    .card-container {
+      height: 420px;
+      width: 80%;
+      margin: 0 auto;
+      border-radius: 15px;
+      background-color: #fff;
+      margin-top: 5%;
+      box-shadow: 0 5px 20px 0 rgba(57, 115, 230, 0.1);
+      cursor: pointer;
+      border: 1px solid transparent;
+      transition: transform 500ms;
+      box-sizing: border-box;
+
+      .img-container {
         text-align: center;
         align-items: center;
+
         img {
-            width: 50%;
-            height: 50%;
+          width: 50%;
+          height: 50%;
         }
-    }
-    .card-title {
+      }
+
+      .card-title {
         font-size: 2rem;
-        color: #4D4D4D;
+        color: #4d4d4d;
         font-weight: 500;
         padding-top: 15px;
-    }
-    .card-description {
-         font-family: National-Book;
-         color: #666666;
-         font-size: 1.5rem;
-         letter-spacing: 0;
-         line-height: 28px;
-         padding: 15px;
-    }
-    .learn-more {
-        font-family: National-Medium;
+      }
+
+      .card-description {
+        font-family: 'National-Book', sans-serif;
+        color: #666;
+        font-size: 1.5rem;
+        letter-spacing: 0;
+        line-height: 28px;
+        padding: 15px;
+      }
+
+      .learn-more {
+        font-family: 'National-Medium', sans-serif;
         margin-top: 10px;
+
         .learn-more-text {
-            color: #3973E6;
-            font-size: 1.5rem;
+          color: #3973e6;
+          font-size: 1.5rem;
         }
+      }
     }
   }
-}
 }
 
 @media only screen and (max-width: 27rem) {
-.link-card {
-text-decoration: none;
-text-align: center;
-.card-container {
-    height: 420px;
-    width: 90%;
-    margin: 0 auto;
-    border-radius: 15px;
-    background-color: #FFFFFF;
-    margin-top: 5%;
-    box-shadow: 0 5px 20px 0 rgba(57, 115, 230, 0.1);
-    cursor: pointer;
-    border: 1px solid transparent;
-    transition: transform 500ms;
-    box-sizing: border-box;
-    .img-container {
+  .link-card {
+    text-decoration: none;
+    text-align: center;
+
+    .card-container {
+      height: 420px;
+      width: 90%;
+      margin: 0 auto;
+      border-radius: 15px;
+      background-color: #fff;
+      margin-top: 5%;
+      box-shadow: 0 5px 20px 0 rgba(57, 115, 230, 0.1);
+      cursor: pointer;
+      border: 1px solid transparent;
+      transition: transform 500ms;
+      box-sizing: border-box;
+
+      .img-container {
         text-align: center;
         align-items: center;
+
         img {
-            width: 80%;
-            height: 80%;
+          width: 80%;
+          height: 80%;
         }
-    }
-    .card-title {
+      }
+
+      .card-title {
         font-size: 1.5rem;
-        color: #4D4D4D;
+        color: #4d4d4d;
         font-weight: 500;
         padding-top: 15px;
-    }
-    .card-description {
-         font-family: National-Book;
-         color: #666666;
-         font-size: 1.25rem;
-         letter-spacing: 0;
-         line-height: 28px;
-         padding: 15px;
-    }
-    .learn-more {
-        font-family: National-Medium;
+      }
+
+      .card-description {
+        font-family: 'National-Book', sans-serif;
+        color: #666;
+        font-size: 1.25rem;
+        letter-spacing: 0;
+        line-height: 28px;
+        padding: 15px;
+      }
+
+      .learn-more {
+        font-family: 'National-Medium', sans-serif;
         margin-top: 10px;
+
         .learn-more-text {
-            color: #3973E6;
-            font-size: 1.25rem;
+          color: #3973e6;
+          font-size: 1.25rem;
         }
+      }
     }
   }
 }
-}  
 </style>
