b2a56135b21545c2d5d5c4001be1bf4633d09455
Asif Ahmed <asif.ahmed@freshworks.com>
2020-08-28 12:10:00 +0530

    test(fw-modal): added test cases

--- src/components/modal/modal.e2e.ts
+++ src/components/modal/modal.e2e.ts
@@ -8,4 +8,27 @@ describe('fw-modal', () => {
     const element = await page.find('fw-modal');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('triggers fwClose when visible property is changed to false', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<fw-modal visible></fw-modal>');
+    const element = await page.find('fw-modal');
+    const fwClosed = await page.spyOnEvent('fwClosed');
+    element.setAttribute('visible', false);
+    await page.waitForChanges();
+    expect(fwClosed).toHaveReceivedEvent();
+  });
+
+  it('triggers fwAction when Action Button is clicked', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent('<fw-modal visible></fw-modal>');
+    await page.waitForChanges();
+    const element = await page.findAll('fw-modal >>> fw-button');
+    const fwAction = await page.spyOnEvent('fwAction');
+    await element[1].click();
+    await page.waitForChanges();
+    expect(fwAction).toHaveReceivedEvent();
+  });
 });
