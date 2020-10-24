4ef61630116234fa6bba64e8e64ed0143e3a9905
existentialcoder <shravanayyappa@gmail.com>
2020-10-24 20:32:31 +0530

    fix(icon): minor gulp file fix
    Minor gulp file fix
    
    fix #143

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const buildIconsJson = async () => {
-    const iconComponentDir = './src/components/icon';
+    const iconComponentPath = './src/components/icon';
+    const iconAssetsPath = '/icon-assets/icons';
+    const iconsJsonFile = 'icons.json';
+
     const iconsJson = {};
     const getIconsJson = async () => {
         try {
-            const allSvgFiles = await fs.readdir(path.join(iconComponentDir, '/icon-assets/icons/'));
+            const allSvgFiles = await fs.readdir(path.join(iconComponentPath, iconAssetsPath));
             for (const svgFile of allSvgFiles) {
-                const svgFilePath = path.join(iconComponentDir, '/icon-assets/icons/', svgFile);
+                const svgFilePath = path.join(iconComponentPath, iconAssetsPath, svgFile);
                 const svgContent = await fs.readFile(svgFilePath);
                 
                 const svgName = path.parse(svgFile).name;
@@ -22,13 +25,11 @@ const buildIconsJson = async () => {
 
     try {
         const iconsJson = await getIconsJson();
-        fs.writeFile(path.join(iconComponentDir, '/icons.json'), JSON.stringify(iconsJson));
-        console.log('Succesfully written icons.json');
+        fs.writeFile(path.join(iconComponentPath, iconsJsonFile), JSON.stringify(iconsJson));
+        console.log(`Succesfully written ${iconsJsonFile}`);
     } catch (ex) {
-        console.error(`Exception occured while building svg json: ${JSON.stringify(ex)}`);
+        console.error(`Exception occured while building ${iconsJsonFile}: ${JSON.stringify(ex)}`);
     }
 };
-
-buildIconsJson();
 
 exports.default = buildIconsJson;
