dc1e63a5945627c02bba8ec1f0e861c87dfab7ca
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-09-23 23:14:59 +0530

    Updated Prettier Eslint config and Ally plugin for Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 'latest', // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
     useJSXTextNode: true,
     ecmaFeatures: {
@@ -17,28 +17,14 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
-    'prettier',
   ],
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'jsx-a11y'],
   rules: {
-    'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'es5',
-        endOfLine: 'auto',
-        allowParens: 0,
-        printWidth: 80,
-      },
-    ],
     '@typescript-eslint/no-unused-vars': 0,
     'react/no-jsx-bind': 0,
-  },
-  settings: {
-    'import/core-modules': ['@stencil/core/testing'],
   },
 };
