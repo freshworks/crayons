e95d468962c768c3261fbda84660425cfa242809
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-03 13:07:07 +0530

    chore: set es5 build to true

--- packages/crayons-core/stencil.config.ts
+++ packages/crayons-core/stencil.config.ts
@@ -71,7 +71,7 @@ export const config: Config = {
     ],
     coverageReporters: ['json', 'lcov', 'text', 'clover', 'text-summary'],
   },
-  buildEs5: 'prod',
+  buildEs5: true,
   extras: {
     appendChildSlotFix: true,
     cssVarsShim: true,
