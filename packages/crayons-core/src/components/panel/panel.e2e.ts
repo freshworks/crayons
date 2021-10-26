1ea764a0d896a2a894dc5987b14e6a5b65c477e8
Kulkarni Shravan Kashyap <kulkarni.kashyap@freshworks.com>
2021-10-26 15:56:52 +0530

    test(crayons-core): adding test to verify rendering of child components for fw-panel

--- packages/crayons-core/src/components/panel/panel.e2e.ts
+++ packages/crayons-core/src/components/panel/panel.e2e.ts
@@ -16,4 +16,43 @@ describe('fw-panel', () => {
     const element = await page.find('fw-panel');
     expect(element.shadowRoot).toEqualHtml(`<slot></slot>`);
   });
+
+  it('renders child components', async () => {
+    const page = await newE2EPage();
+    const expectedContent = `
+      <div>
+        <fw-input
+          class="hydrated"
+          label='Name'
+          icon-left="add-contact"
+          state-text="Do not enter your user ID"
+          state="warning"
+          placeholder="Enter your official name"
+          required
+          clear-input
+        >
+          <input class="hidden-input" type="hidden" value="">
+        </fw-input>
+        <fw-button class="hydrated" color="secondary" role="button">Submit</fw-button>
+        <fw-button class="hydrated" color="secondary" role="button">Save</fw-button>
+      </div>`;
+    await page.setContent(
+      `<fw-panel>
+        <div>
+          <fw-input
+            label='Name' icon-left="add-contact" state-text="Do not enter your user ID"
+            state="warning"
+            placeholder="Enter your official name"
+            required
+            clear-input
+          >
+          </fw-input>
+          <fw-button color="secondary">Submit</fw-button>
+          <fw-button color="secondary">Save</fw-button>
+        </div>
+    </fw-panel>`
+    );
+    const element = await page.find('fw-panel');
+    expect(element.innerHTML).toEqualHtml(expectedContent);
+  });
 });
