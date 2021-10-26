69d911f191c6f8365b6aa24be969a6732bd87901
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-10-26 10:42:52 +0530

    chore(release): lerna release scripts changes
    update package lock file and clean up empty line and version bump text only in change logs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,3 @@
-const {
-  utils: { getPackages },
-} = require('@commitlint/config-lerna-scopes');
-
 module.exports = {
-  extends: [
-    '@commitlint/config-conventional',
-    '@commitlint/config-lerna-scopes',
-  ],
-  rules: {
-    'scope-enum': async (ctx) => [
-      2,
-      'always',
-      [
-        ...(await getPackages(ctx)),
-        // Insert custom scopes below:
-        'root',
-        'release',
-      ],
-    ],
-  },
+  extends: ['@commitlint/config-conventional'],
 };
