cfdc171a14fec390427b0678cd69d64ea3683f5c
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-12-02 11:41:47 +0530

    fix(toast): fix custom template event handling

--- packages/crayons-core/src/components/toast/toast-util.ts
+++ packages/crayons-core/src/components/toast/toast-util.ts
@@ -1,3 +1,4 @@
+import { cloneNodeWithEvents } from '../../utils';
 export interface ToastOptions {
   /**
    * The Content of the action link
@@ -77,8 +78,10 @@ export function createToastNotification(
   let toastElem;
 
   if (opts.contentref) {
-    toastElem = document.querySelector(opts.contentref);
+    const ref = document.querySelector(opts.contentref);
     //toastElem = ref.cloneNode(true);
+    // since we are cloning nodes, the events don't get cloned by default. we have to copy the events manually
+    toastElem = cloneNodeWithEvents(ref, true, true);
     props.content = '';
   } else {
     toastElem = document.createElement('fw-toast-message');
