ff542cb656d5aa01846a365bb2d1507db6ddda7c
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2021-12-07 23:50:43 +0530

    test(format-number): updated testcases for format-number

--- packages/crayons-core/src/components/format-number/format-number.e2e.ts
+++ packages/crayons-core/src/components/format-number/format-number.e2e.ts
@@ -8,4 +8,24 @@ describe('fw-format-number', () => {
     const element = await page.find('fw-format-number');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('renders number based on locale', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      '<fw-format-number value="2000" locale="de" minimum-fraction-digits="2"></fw-format-number>'
+    );
+    const element = await page.find('fw-format-number');
+    expect(element.shadowRoot).toEqualText('2.000');
+  });
+
+  it('renders currency based on locale', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(
+      '<fw-format-number type="currency" currency="GBP" value="5000" locale="en-GB"></fw-format-number>'
+    );
+    const element = await page.find('fw-format-number');
+    expect(element.shadowRoot).toEqualText('£5,000');
+  });
 });
