7c93046c0196fd2f279ba073d563e345fba1bdfe
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-03-24 10:54:38 +0530

    docs(docs): docs changes

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,66 +1,67 @@
 const path = require("path");
-const components = require('../custom-elements.json');
-const version = require("../../package.json").version
+const components = require("../custom-elements.json");
+const version = require("../../package.json").version;
 
-const getComponents = () => components.tags.map(({ tag }) => {
-  return `/components/${tag.substr(3)}/`;
-});
+const getComponents = () =>
+  components.tags.map(({ tag }) => {
+    return `/components/${tag.substr(3)}/`;
+  });
 
 const getTags = () => [
- 'Web Components',
- 'Ui Kit',
- 'Marketplace',
- 'Freshworks Marketplace',
- 'Component Library',
- 'FDK',
- 'Freshworks developers',
- 'Freshworks development',
- 'Freshworks',
- 'Freshworks Development Kit',
+  "Web Components",
+  "Ui Kit",
+  "Marketplace",
+  "Freshworks Marketplace",
+  "Component Library",
+  "FDK",
+  "Freshworks developers",
+  "Freshworks development",
+  "Freshworks",
+  "Freshworks Development Kit",
 ];
 
-const websiteUrl = 'https://crayons.freshworks.com/v2';
+const websiteUrl = "https://crayons.freshworks.com/v2";
 
 module.exports = {
-  title: 'Crayons',
-  base: '/v2/',
-  description: 'A refreshed design library for the Freshworks Developers.',
-  dest: 'docs-dist',
-  docsDir: 'src',
+  title: "Crayons",
+  base: "/v2/",
+  description: "A refreshed design library for the Freshworks Developers.",
+  dest: "docs-dist",
+  docsDir: "src",
   head: [
-    ['script', { type: 'module', src: '/www/build/crayons.esm.js' }],
-    ['script', {  nomodule:'', src: '/www/build/crayons.js' }],
-    ['link', { rel: 'icon', href: '/favicon.png' }]
+    ["script", { type: "module", src: "/www/build/crayons.esm.js" }],
+    ["script", { nomodule: "", src: "/www/build/crayons.js" }],
+    ["link", { rel: "icon", href: "/favicon.png" }],
   ],
   themeConfig: {
-    lastUpdated: 'Last Updated',
+    lastUpdated: "Last Updated",
     smoothScroll: true,
     sidebar: [
       {
-        title: 'Introduction',
+        title: "Introduction",
         collapsable: false,
         sidebarDepth: 1,
-        children: [
-          '/components/',
-        ]
+        children: ["/components/"],
       },
       {
-        title: 'Components',
+        title: "Components",
         collapsable: false,
-        sidebarDepth: 1, 
-        children: getComponents()
-      }
+        sidebarDepth: 1,
+        children: getComponents(),
+      },
     ],
     nav: [
       {
-        text: 'Migrating to v3',
-        link: 'https://crayons.freshworks.com/introduction/migrating-to-v3/',
+        text: "Migrating to v3",
+        link: "https://crayons.freshworks.com/introduction/migrating-to-v3/",
       },
       {
         text: `v${version?.split(".")[0]}.x`,
-        items: ['v3.x', 'v2.x'].map((version) => ({
-          text: version,
-          link: `https://crayons.freshworks.com/${version?.split(".")[0]}`,
+        items: ["v3.x", "v2.x"].map((v) => ({
+          text: v,
+          link: `https://crayons.freshworks.com/${
+            v === `v${version?.split(".")[0]}.x` ? `${v?.split(".")[0]}/` : ""
+          }`,
         })),
       },
     ],
@@ -70,34 +71,41 @@ module.exports = {
       "live",
       {
         layout: path.resolve(__dirname, "./previewLayout.vue"),
-        squiggles: false
-      }
+        squiggles: false,
+      },
     ],
-    ['@vuepress/active-header-links'],
-    ['vuepress-plugin-google-tag-manager', {
-      gtm: 'GTM-WQKBJ88'
-    }],
+    ["@vuepress/active-header-links"],
     [
-      'vuepress-plugin-seo', {
-        siteTitle: () => 'Crayons',
-        title: () => 'Crayons',
-        description: () => 'A refreshed design library for the Freshworks Developers.',
-        author: () => 'Freshworks',
+      "vuepress-plugin-google-tag-manager",
+      {
+        gtm: "GTM-WQKBJ88",
+      },
+    ],
+    [
+      "vuepress-plugin-seo",
+      {
+        siteTitle: () => "Crayons",
+        title: () => "Crayons",
+        description: () =>
+          "A refreshed design library for the Freshworks Developers.",
+        author: () => "Freshworks",
         tags: getTags,
-        twitterCard: _ => 'summary_large_image',
-        type: () => 'website',
+        twitterCard: (_) => "summary_large_image",
+        type: () => "website",
         url: (_, $site, path) => websiteUrl + path,
-        image: ($page, $site) => 'https://s3.amazonaws.com/static.freshcloud.io/crayons/assets/crayons.png',
-        publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
-        modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated),
-      }
-    ]
+        image: ($page, $site) =>
+          "https://s3.amazonaws.com/static.freshcloud.io/crayons/assets/crayons.png",
+        publishedAt: ($page) =>
+          $page.frontmatter.date && new Date($page.frontmatter.date),
+        modifiedAt: ($page) => $page.lastUpdated && new Date($page.lastUpdated),
+      },
+    ],
   ],
   configureWebpack: {
     resolve: {
       alias: {
-        '@components': path.resolve(__dirname, '../components')
-      }
-    }
-  }
-}
+        "@components": path.resolve(__dirname, "../components"),
+      },
+    },
+  },
+};
