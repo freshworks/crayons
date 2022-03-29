5e211b27361e38caaad8eb86f4e6fd58eeacaaea
Kishore Kumar <kishore.kumar@freshworks.com>
2022-03-29 10:01:56 +0530

    feat(fw-accordion-header): added prop for different icon size (#518)

--- packages/crayons-core/src/components.d.ts
+++ packages/crayons-core/src/components.d.ts
@@ -31,6 +31,10 @@ export namespace Components {
     }
     interface FwAccordionTitle {
         "expanded": boolean;
+        /**
+          * The size of the default icon
+         */
+        "iconSize": 'small' | 'medium' | 'large';
         "toggleState": any;
         /**
           * Truncate title on text overflow
@@ -2270,6 +2274,10 @@ declare namespace LocalJSX {
     }
     interface FwAccordionTitle {
         "expanded"?: boolean;
+        /**
+          * The size of the default icon
+         */
+        "iconSize"?: 'small' | 'medium' | 'large';
         "toggleState"?: any;
         /**
           * Truncate title on text overflow
