9debc221f7c3cb631906d06db7c53ef523d16f3f
Kishore Kumar <kishore.kumar@freshworks.com>
2022-04-25 17:13:40 +0530

    fix(fw-select): added povision to add/update the options dynamically

--- packages/crayons-core/src/components/select/select.tsx
+++ packages/crayons-core/src/components/select/select.tsx
@@ -339,6 +339,21 @@ export class Select {
     this.renderInput();
   }
 
+  @Watch('options')
+  onOptionsChange(newValue) {
+    this.dataSource = newValue;
+    const selectedValues = newValue?.filter((option) => option.selected);
+    if (selectedValues.length > 0) {
+      // selected flag is available in the datasource
+      this.selectedOptionsState = selectedValues;
+      this.value = this.multiple
+        ? this.selectedOptionsState[0]?.value
+        : this.selectedOptionsState.map((x) => x.value);
+    } else {
+      this.value = this.multiple ? [] : '';
+    }
+  }
+
   @Method()
   async getSelectedItem(): Promise<any> {
     return this.fwListOptions.getSelectedOptions();
