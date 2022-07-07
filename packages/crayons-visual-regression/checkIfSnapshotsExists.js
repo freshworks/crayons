8c713f34f8f05482c15ae46272db6dbc63cba596
rihansiddhi <rihansiddhiofficial@gmail.com>
2022-07-07 17:07:42 +0530

    fix: add constant for total snapshots

--- packages/crayons-visual-regression/checkIfSnapshotsExists.js
+++ packages/crayons-visual-regression/checkIfSnapshotsExists.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { exec } = require('child_process');
+const TOTAL_SNAPSHOTS = 139;
 
 if (fs.existsSync('./__tests__/__snapshots__')) {
   exec(
@@ -13,12 +14,12 @@ if (fs.existsSync('./__tests__/__snapshots__')) {
         console.log(`stderr: ${stderr}`);
         return;
       }
-      if (parseInt(stdout.trimStart()) === 139) {
+      if (parseInt(stdout.trimStart()) === TOTAL_SNAPSHOTS) {
         console.log('Total snapshots :', parseInt(stdout.trimStart()));
         process.exit(0);
       } else {
         console.error(
-          `Error: Build Failed."crayons-visual-regression/__tests__/__snapshots__" folder contains only ${stdout.trimStart()} snapshots out of 125 snapshots. Kindly pull/rebase from repo.`
+          `Error: Build Failed."crayons-visual-regression/__tests__/__snapshots__" folder contains only ${stdout.trimStart()} snapshots out of ${TOTAL_SNAPSHOTS} snapshots. Kindly pull/rebase from repo.`
         );
         process.exit(1);
       }
