04a1d05a4ebb8d011fff452509ebd73858748c5d
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-08-19 11:11:36 +0530

    fix(list-options): added margin between search input and options list

--- packages/crayons-core/src/components/options-list/list-options.tsx
+++ packages/crayons-core/src/components/options-list/list-options.tsx
@@ -409,6 +409,7 @@ export class ListOptions {
   renderSearchInput() {
     return (
       <fw-input
+        class='input-search'
         ref={(searchInput) => (this.searchInput = searchInput)}
         placeholder={this.searchText}
         onInput={() => this.handleSearchWithDebounce(this.searchInput.value)}
