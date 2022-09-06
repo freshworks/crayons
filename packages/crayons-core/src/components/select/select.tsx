0c460209d3f71ebb2493a38d76269cb73790cdf8
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-09-06 22:04:26 +0530

    feat(select): prevent the options from being clipped in a container with overflow:auto|hidden|scroll

--- packages/crayons-core/src/components/select/select.tsx
+++ packages/crayons-core/src/components/select/select.tsx
@@ -230,6 +230,12 @@ export class Select {
    */
   @Prop() boundary: HTMLElement;
 
+  /**
+   *  Option to prevent the select options from being clipped when the component is placed inside a container with
+   * `overflow: auto|hidden|scroll`.
+   */
+  @Prop() hoist = false;
+
   // Events
   /**
    * Triggered when a value is selected or deselected from the list box options.
@@ -647,6 +653,7 @@ export class Select {
               same-width={this.sameWidth}
               placement={this.optionsPlacement}
               boundary={this.boundary}
+              hoist={this.hoist}
             >
               <div
                 slot='popover-trigger'
