da92adbcff79a292b87171abdc4adc429e935577
rihansiddhi <rihansiddhiofficial@gmail.com>
2022-09-12 11:12:09 +0530

    refactor: remove util function for validate email

--- packages/crayons-core/src/utils/index.ts
+++ packages/crayons-core/src/utils/index.ts
@@ -383,10 +383,3 @@ export const popperModifierRTL = {
     }
   },
 };
-
-export const validateEmail = (email: string) =>
-  String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
