835fafa678a61e99da83b5ef591f2d062bcb3c6e
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-09-27 12:00:48 +0530

    docs(v3): add basepath as /v3/

--- www/.vuepress/config.js
+++ www/.vuepress/config.js
@@ -56,7 +56,7 @@ const websiteUrl = 'https://crayons.freshworks.com';
 
 module.exports = {
   title: 'Crayons',
-  base: '/',
+  base: '/v3/',
   description: 'A refreshed design library for the Freshworks Developers.',
   dest: 'www-dist',
   head: [...headScripts, ['link', { rel: 'icon', href: '/favicon.png' }]],
