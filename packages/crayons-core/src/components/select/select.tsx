1d5a3e7ee9318b28185cf3d070e29a47ae98eca3
rihansiddhi <rihansiddhiofficial@gmail.com>
2022-11-03 10:10:22 +0530

    fix: fix height of avatar variant of multi select

--- packages/crayons-core/src/components/select/select.tsx
+++ packages/crayons-core/src/components/select/select.tsx
@@ -939,7 +939,7 @@ export class Select {
                     <div class='input-container-inner'>
                       {this.multiple && (
                         <div
-                          class='tag-container'
+                          class={`tag-container ${this.tagVariant}`}
                           onFocus={this.focusOnTagContainer}
                           ref={(tagContainer) =>
                             (this.tagContainer = tagContainer)
