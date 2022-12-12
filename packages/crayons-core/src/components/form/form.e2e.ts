cd101d40bf1865a87736e810bdba233b7859310e
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-12-12 21:38:12 +0530

    test(form): added tests for setFieldSearchText - filtering form fields via form method

--- packages/crayons-core/src/components/form/form.e2e.ts
+++ packages/crayons-core/src/components/form/form.e2e.ts
@@ -479,4 +479,62 @@ describe('fw-form', () => {
       fieldOptionsData.formSchema.fields[0].choices[1][key]
     );
   });
+
+  it('Should filter form fields on calling setFieldSearchText method on the form', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-form></fw-form>`);
+
+    await page.$eval(
+      'fw-form',
+      (elm: any, { formSchema }) => {
+        elm.formSchema = formSchema;
+        elm.initialValues = {
+          pincode: '123345',
+          amount_paid: '10',
+        };
+      },
+      props
+    );
+
+    await page.waitForChanges();
+
+    const element = await page.find('fw-form');
+
+    await element.callMethod('setFieldSearchText', 'Pin');
+
+    await page.waitForChanges();
+    expect(
+      element.shadowRoot.querySelectorAll('fw-form-control').length
+    ).toEqual(1);
+  });
+
+  it('Should render all the form fields on calling setFieldSearchText method on the form with a empty string or null/undefined', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(`<fw-form></fw-form>`);
+
+    await page.$eval(
+      'fw-form',
+      (elm: any, { formSchema }) => {
+        elm.formSchema = formSchema;
+        elm.initialValues = {
+          pincode: '123345',
+          amount_paid: '10',
+        };
+      },
+      props
+    );
+
+    await page.waitForChanges();
+
+    const element = await page.find('fw-form');
+
+    await element.callMethod('setFieldSearchText', '');
+
+    await page.waitForChanges();
+    expect(
+      element.shadowRoot.querySelectorAll('fw-form-control').length
+    ).toEqual(6);
+  });
 });
