12d660795013be8a6d08a5957a48e0e87e9c0bed
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2023-01-11 10:40:55 +0530

    chore(ci): ci

--- .github/delete-pages.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf $1
-
-HUSKY_SKIP_HOOKS=1 git add .
-HUSKY_SKIP_HOOKS=1 git commit -m "Delete directory that is not required"
-HUSKY_SKIP_HOOKS=1 git push origin gh-pages
