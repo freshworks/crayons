1cb29fc47a4f54144d4772be97ea8f45252830fa
arvindanta <arvindanta@users.noreply.github.com>
2023-01-12 02:33:02 +0000

    Deploying to gh-pages from @ freshworks/crayons@d3f3e3f1c08ba424bce1d345010690c7baad7e5a 🚀

---
+++ 774/crayons/sw.js
@@ -0,0 +1,481 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "e985a75c7e0ba88f2c4a3b7901b4ec98"
+  },
+  {
+    "url": "build/index.esm.js",
+    "revision": "ec48d532f3dbe2cdbdda4fd5e136fabc"
+  },
+  {
+    "url": "build/p-009162cf.js"
+  },
+  {
+    "url": "build/p-07dc799d.js"
+  },
+  {
+    "url": "build/p-08f70d34.js"
+  },
+  {
+    "url": "build/p-0a4050b0.js"
+  },
+  {
+    "url": "build/p-0a5db85c.js"
+  },
+  {
+    "url": "build/p-0ba0fef2.entry.js"
+  },
+  {
+    "url": "build/p-0f0602ca.entry.js"
+  },
+  {
+    "url": "build/p-0f17b871.js"
+  },
+  {
+    "url": "build/p-0fb04365.js"
+  },
+  {
+    "url": "build/p-112455b1.js"
+  },
+  {
+    "url": "build/p-13dd0a66.js"
+  },
+  {
+    "url": "build/p-161dda7f.entry.js"
+  },
+  {
+    "url": "build/p-1a019790.js"
+  },
+  {
+    "url": "build/p-1a6d02aa.entry.js"
+  },
+  {
+    "url": "build/p-1b3a7634.js"
+  },
+  {
+    "url": "build/p-1e4f6cab.js"
+  },
+  {
+    "url": "build/p-1f0d3038.js"
+  },
+  {
+    "url": "build/p-200fa217.js"
+  },
+  {
+    "url": "build/p-20dd2cf7.js"
+  },
+  {
+    "url": "build/p-219dbc1b.js"
+  },
+  {
+    "url": "build/p-2303425d.js"
+  },
+  {
+    "url": "build/p-255e6387.js"
+  },
+  {
+    "url": "build/p-26f83dbc.js"
+  },
+  {
+    "url": "build/p-27ddf772.js"
+  },
+  {
+    "url": "build/p-2cde8d57.js"
+  },
+  {
+    "url": "build/p-2d7f10cc.entry.js"
+  },
+  {
+    "url": "build/p-2e74b399.entry.js"
+  },
+  {
+    "url": "build/p-2f03632b.entry.js"
+  },
+  {
+    "url": "build/p-2fb8118f.entry.js"
+  },
+  {
+    "url": "build/p-30848d13.js"
+  },
+  {
+    "url": "build/p-320b781c.js"
+  },
+  {
+    "url": "build/p-325c5933.js"
+  },
+  {
+    "url": "build/p-32abb7ce.js"
+  },
+  {
+    "url": "build/p-39c8189c.js"
+  },
+  {
+    "url": "build/p-3a53101b.js"
+  },
+  {
+    "url": "build/p-3e46c710.js"
+  },
+  {
+    "url": "build/p-4038fd61.js"
+  },
+  {
+    "url": "build/p-40702513.js"
+  },
+  {
+    "url": "build/p-4128f6a4.js"
+  },
+  {
+    "url": "build/p-421ef933.js"
+  },
+  {
+    "url": "build/p-42dd29c6.js"
+  },
+  {
+    "url": "build/p-44adbfd6.entry.js"
+  },
+  {
+    "url": "build/p-482b91b0.entry.js"
+  },
+  {
+    "url": "build/p-4879646b.js"
+  },
+  {
+    "url": "build/p-4af39067.entry.js"
+  },
+  {
+    "url": "build/p-4c2b452b.entry.js"
+  },
+  {
+    "url": "build/p-4ea25f4c.js"
+  },
+  {
+    "url": "build/p-4f410520.js"
+  },
+  {
+    "url": "build/p-51b23529.js"
+  },
+  {
+    "url": "build/p-51ee0990.entry.js"
+  },
+  {
+    "url": "build/p-53467aff.js"
+  },
+  {
+    "url": "build/p-542bab7b.js"
+  },
+  {
+    "url": "build/p-58a566ac.js"
+  },
+  {
+    "url": "build/p-5965b5d6.entry.js"
+  },
+  {
+    "url": "build/p-5d2580f0.js"
+  },
+  {
+    "url": "build/p-5d838bb3.js"
+  },
+  {
+    "url": "build/p-64d88eaa.js"
+  },
+  {
+    "url": "build/p-687ce7d3.js"
+  },
+  {
+    "url": "build/p-6cb2598c.js"
+  },
+  {
+    "url": "build/p-704d8cf2.js"
+  },
+  {
+    "url": "build/p-70d12b2f.js"
+  },
+  {
+    "url": "build/p-7469878a.entry.js"
+  },
+  {
+    "url": "build/p-79178ffe.js"
+  },
+  {
+    "url": "build/p-79292df1.js"
+  },
+  {
+    "url": "build/p-7966f5b6.js"
+  },
+  {
+    "url": "build/p-7a2fba59.js"
+  },
+  {
+    "url": "build/p-7d0821bd.js"
+  },
+  {
+    "url": "build/p-7d408320.js"
+  },
+  {
+    "url": "build/p-7d600ddf.entry.js"
+  },
+  {
+    "url": "build/p-833bac36.js"
+  },
+  {
+    "url": "build/p-8467cac2.js"
+  },
+  {
+    "url": "build/p-847a2727.js"
+  },
+  {
+    "url": "build/p-88d3a148.js"
+  },
+  {
+    "url": "build/p-88ecf0e4.js"
+  },
+  {
+    "url": "build/p-89538093.js"
+  },
+  {
+    "url": "build/p-8996a612.js"
+  },
+  {
+    "url": "build/p-8997d9ec.js"
+  },
+  {
+    "url": "build/p-8a37ccd5.entry.js"
+  },
+  {
+    "url": "build/p-8aa37858.js"
+  },
+  {
+    "url": "build/p-8b71e7f0.js"
+  },
+  {
+    "url": "build/p-8d4a6bf9.js"
+  },
+  {
+    "url": "build/p-8d764813.entry.js"
+  },
+  {
+    "url": "build/p-8eac5a5d.entry.js"
+  },
+  {
+    "url": "build/p-919ec84c.js"
+  },
+  {
+    "url": "build/p-924d8982.js"
+  },
+  {
+    "url": "build/p-931aae41.js"
+  },
+  {
+    "url": "build/p-93d11a89.js"
+  },
+  {
+    "url": "build/p-9555078d.js"
+  },
+  {
+    "url": "build/p-9e2990f1.js"
+  },
+  {
+    "url": "build/p-a0bea2a0.entry.js"
+  },
+  {
+    "url": "build/p-a1f63360.js"
+  },
+  {
+    "url": "build/p-a84951eb.entry.js"
+  },
+  {
+    "url": "build/p-a9ac29d3.js"
+  },
+  {
+    "url": "build/p-a9ded7f3.js"
+  },
+  {
+    "url": "build/p-acd36a3b.js"
+  },
+  {
+    "url": "build/p-ae8cca33.js"
+  },
+  {
+    "url": "build/p-b049ca0a.entry.js"
+  },
+  {
+    "url": "build/p-b092120b.js"
+  },
+  {
+    "url": "build/p-b208e446.js"
+  },
+  {
+    "url": "build/p-b2cf2c69.js"
+  },
+  {
+    "url": "build/p-b6e0e896.entry.js"
+  },
+  {
+    "url": "build/p-b8c7d9de.entry.js"
+  },
+  {
+    "url": "build/p-bd904665.entry.js"
+  },
+  {
+    "url": "build/p-c181e412.entry.js"
+  },
+  {
+    "url": "build/p-c20e1529.js"
+  },
+  {
+    "url": "build/p-c328ec17.js"
+  },
+  {
+    "url": "build/p-c48a647a.js"
+  },
+  {
+    "url": "build/p-c5706edf.js"
+  },
+  {
+    "url": "build/p-c79015f5.js"
+  },
+  {
+    "url": "build/p-c87d4a3c.js"
+  },
+  {
+    "url": "build/p-cbb6d02c.js"
+  },
+  {
+    "url": "build/p-ce342f70.js"
+  },
+  {
+    "url": "build/p-cee7dcc8.js"
+  },
+  {
+    "url": "build/p-d05dcfdf.entry.js"
+  },
+  {
+    "url": "build/p-d4599615.js"
+  },
+  {
+    "url": "build/p-d4c6e1dc.js"
+  },
+  {
+    "url": "build/p-d53ff545.js"
+  },
+  {
+    "url": "build/p-d5540224.js"
+  },
+  {
+    "url": "build/p-d7293421.js"
+  },
+  {
+    "url": "build/p-d85e5927.js"
+  },
+  {
+    "url": "build/p-d8dbf8c2.entry.js"
+  },
+  {
+    "url": "build/p-da3480fb.js"
+  },
+  {
+    "url": "build/p-da558c5a.js"
+  },
+  {
+    "url": "build/p-daeb2d90.js"
+  },
+  {
+    "url": "build/p-dd036e2d.entry.js"
+  },
+  {
+    "url": "build/p-dd7e2241.js"
+  },
+  {
+    "url": "build/p-dea85efc.js"
+  },
+  {
+    "url": "build/p-e0bb9eac.entry.js"
+  },
+  {
+    "url": "build/p-e60d82bc.entry.js"
+  },
+  {
+    "url": "build/p-e61b4569.js"
+  },
+  {
+    "url": "build/p-e61f3bb3.js"
+  },
+  {
+    "url": "build/p-e7d0d932.entry.js"
+  },
+  {
+    "url": "build/p-ee1bdad1.js"
+  },
+  {
+    "url": "build/p-ef69aec3.js"
+  },
+  {
+    "url": "build/p-f0666c70.js"
+  },
+  {
+    "url": "build/p-f0da6985.js"
+  },
+  {
+    "url": "build/p-f141cdcd.js"
+  },
+  {
+    "url": "build/p-f38a8df4.js"
+  },
+  {
+    "url": "build/p-f3bec3af.entry.js"
+  },
+  {
+    "url": "build/p-f51ff291.entry.js"
+  },
+  {
+    "url": "build/p-f634fa40.js"
+  },
+  {
+    "url": "build/p-f646d6e1.js"
+  },
+  {
+    "url": "build/p-f689b48a.js"
+  },
+  {
+    "url": "build/p-f6f91c8c.js"
+  },
+  {
+    "url": "build/p-f7242548.js"
+  },
+  {
+    "url": "build/p-f8410bc0.js"
+  },
+  {
+    "url": "build/p-f8d9cbd7.entry.js"
+  },
+  {
+    "url": "build/p-f9d2f52b.js"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
