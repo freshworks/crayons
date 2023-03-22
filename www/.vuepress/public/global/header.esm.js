127d6b2157f3a293edb25f5da0be04047ab1b5bd
Arvindan T A <arvindan.aswathanarayanan@freshworks.com>
2023-03-22 15:33:53 +0530

    docs(form): added Yup based form validation with live examples.Injecting global scripts in docs site (#824)

---
+++ www/.vuepress/public/global/header.esm.js
@@ -0,0 +1,5 @@
+/** This script gets added to the header of the docs site */
+/** Add any script that needs to be executed on load of the site */
+
+import * as Yup from 'https://cdn.skypack.dev/yup@0.32';
+window.Yup = Yup; // setting Yup object on the window to use it in the live code example
