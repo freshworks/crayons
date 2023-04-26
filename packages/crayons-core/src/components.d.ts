b29d755b3a3089135846316f85fe0297d25f51ce
Vignesh Manogar <vignesh.manogar@freshworks.com>
2023-04-26 09:50:41 +0530

    feat(fw-tab): adding spacing css variables to tab component
    - tab css spacing variables. - select-option css variables for font weight and color. - popover
    isActive prop

--- packages/crayons-core/src/components.d.ts
+++ packages/crayons-core/src/components.d.ts
@@ -1498,6 +1498,10 @@ export namespace Components {
          */
         "hoist": boolean;
         /**
+          * Indicates if popup must open on interacting.
+         */
+        "isActive": boolean;
+        /**
           * Placement of the popover content with respect to the popover trigger.
          */
         "placement": PopoverPlacementType;
@@ -4416,6 +4420,10 @@ declare namespace LocalJSX {
           * Option to prevent the tooltip from being clipped when the component is placed inside a container with `overflow: auto|hidden|scroll`.
          */
         "hoist"?: boolean;
+        /**
+          * Indicates if popup must open on interacting.
+         */
+        "isActive"?: boolean;
         /**
           * Triggered whenever the popover contents is closed/hidden.
          */
